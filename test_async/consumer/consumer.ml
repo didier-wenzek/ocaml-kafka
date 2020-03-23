721ec995bdd1f7a7f35ad7c623ea695cc537f297
Marek Kubica <marek@xivilization.net>
2020-03-23 14:48:43 +0100

    Allow picking a custom group_id
    This also prevents the issue that the integration test and the clients
    use the same group_id, thus confusing each other.

--- test_async/consumer/consumer.ml
+++ test_async/consumer/consumer.ml
@@ -3,14 +3,13 @@ open Async
 
 let options =
   [
-    ("metadata.broker.list", "localhost:9092");
-    ("group.id", "test-consumer-groups");
-    ("auto.offset.reset", "earliest");
+    ("metadata.broker.list", "localhost:9092"); ("auto.offset.reset", "earliest");
   ]
 
-let main topic =
+let main (topic, group_id) =
   let open Deferred.Result.Let_syntax in
   Log.Global.debug "Starting";
+  let options = ("group.id", group_id) :: options in
   let%bind consumer = Deferred.return @@ Kafka_async.new_consumer options in
   Log.Global.debug "Got a consumer";
   let%bind reader = Deferred.return @@ Kafka_async.consume consumer ~topic in
@@ -30,8 +29,8 @@ let main topic =
   Log.Global.debug "Destroyed";
   return ()
 
-let main_or_error topic =
-  match%bind main topic with
+let main_or_error opts =
+  match%bind main opts with
   | Ok _ as v -> return v
   | Error (_, msg) -> return @@ Error (Error.of_string msg)
 
@@ -42,6 +41,10 @@ let () =
       let _ = Log.Global.set_level_via_param ()
       and topic =
         flag "topic" (required string) ~doc:"NAME Which topic to consume from"
+      and group_id =
+        flag "group-id"
+          (optional_with_default "ocaml-kafka-async-consumer" string)
+          ~doc:"GROUPID Which group.id to pick for consuming"
       in
-      fun () -> main_or_error topic]
+      fun () -> main_or_error (topic, group_id)]
   |> Command.run
