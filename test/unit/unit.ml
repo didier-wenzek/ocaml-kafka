e8bd88c106bc004dce3c4fbfd595e9d41afa7513
Didier Wenzek <didier.wenzek@acidalie.com>
2020-08-03 17:44:30 +0200

    Remove dummy unit tests

--- test/unit/unit.ml
+++
@@ -1,9 +0,0 @@
-let dummy () = ()
-
-let () =
-  Alcotest.run "Unittests" [
-    "all", [
-      Alcotest.test_case "dummy" `Quick dummy
-
-    ]
-  ]
--- test_lwt/unit/unit.ml
+++
@@ -1,8 +0,0 @@
-let dummy _switch () = Lwt.return ()
-
-let () =
-  Alcotest.run "Lwt Unittests" [
-    "all", [
-      Alcotest_lwt.test_case "dummy" `Quick dummy
-    ]
-  ]
